d7979d30e0b506babda385ecb9d33cfe760f7ce6
nesci <markgagnon28@gmail.com>
2020-12-01 07:58:52 -0500

    2020: Day01

---
+++ 2020/Day01/01.js
@@ -0,0 +1,12 @@
+const inputs = require("./input");
+
+for (let i = 0; i < inputs.length; i++) {
+  const firstEl = inputs[i];
+  for (let j = i + 1; j < inputs.length; j++) {
+    const secondEl = inputs[j];
+
+    if (firstEl + secondEl === 2020) {
+      console.log(firstEl * secondEl);
+    }
+  }
+}
---
+++ 2020/Day01/02.js
@@ -0,0 +1,14 @@
+const inputs = require("./input");
+
+for (let i = 0; i < inputs.length; i++) {
+  const firstEl = inputs[i];
+  for (let j = i + 1; j < inputs.length; j++) {
+    const secondEl = inputs[j];
+    for (let x = j + 1; x < inputs.length; x++) {
+      const thirdEl = inputs[x];
+      if (firstEl + secondEl + thirdEl === 2020) {
+        console.log(firstEl * secondEl * thirdEl);
+      }
+    }
+  }
+}
